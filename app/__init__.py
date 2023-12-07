d743e299f18f4de513ed7d656b51805e87a9b0b7
Владислав Малко <rebelcompaning@gmail.com>
2023-12-08 00:28:20 +0100

    Commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request
+from app.tools import generate_heetmap_by_date, generate_price_range_between_dates
+
+import datetime
 
 app = Flask(__name__)
 
@@ -16,7 +19,31 @@ def timeline():
     return render_template('timeline.html')
 
 # api
-@app.route('/api/v1/hello-world-1')
+@app.route('/api/v1/heat-map-by-date', methods=['GET'])
 def hello_world_1():
-    # return json hello world
-    return {'hello': 'world'}
+    # url example: /api/v1/heat-map-by-date?datetime=2020-01-01
+
+    payload = request.args.to_dict()
+    
+    if payload is None or payload.get('datetime') is None:
+        return 'Please provide a datetime', 400
+
+    date = datetime.datetime.strptime(payload['datetime'], '%Y-%m-%d')
+    return generate_heetmap_by_date(date)
+
+
+@app.route('/api/v1/price-range-between-dates', methods=['GET'])
+def hello_world_2():
+    # url example: /api/v1/price-range-between-dates?start=2020-01-01&end=2020-01-02&country=germany
+
+    payload = request.args.to_dict()
+    
+    if payload is None or payload.get('start') is None or payload.get('end') is None:
+        return 'Please provide a start, end and country', 400
+
+    start = datetime.datetime.strptime(payload['start'], '%Y-%m-%d')
+    end = datetime.datetime.strptime(payload['end'], '%Y-%m-%d')
+    country = payload.get('country', 'germany')
+    property_type = payload.get('property_type', 'apartment')
+
+    return generate_price_range_between_dates(start, end, country, property_type)
--- app/tools.py
+++ app/tools.py
@@ -1,1 +1,71 @@
 # processing tools will be here
+from app.models import SpiderOutputHistory
+from peewee import fn
+import json
+
+
+def generate_heetmap_by_date(date):
+    """
+    Generate heatmap for given date
+    """
+    query = SpiderOutputHistory.select(SpiderOutputHistory.city, fn.COUNT(SpiderOutputHistory.city).alias(
+        'count')).where(fn.date_trunc('day', SpiderOutputHistory.created_at) == date).group_by(SpiderOutputHistory.city)
+
+    all_data = [{'city': x.city, 'count': x.count} for x in query if x.city]
+
+    # in static/cities.json is a list of cities with their coordinates, make [lat, lng, count] list
+    cities = json.load(open('app/static/cities.json', 'r', encoding='utf-8'))
+    cities_dict = {x['name'].lower(): x for x in cities}
+    data = []
+
+    # make count from 0 to 1 for better visualization
+    max_count = max([x['count'] for x in all_data])
+    for x in all_data:
+        x['count'] = x['count'] / max_count
+
+    for x in all_data:
+        city = x['city'].lower()
+        if city in cities_dict:
+            data.append({
+                'lat': cities_dict[city]['lat'],
+                'lng': cities_dict[city]['lng'],
+                'count': x['count'],
+                'name': x['city']
+            })
+
+    return data
+
+
+def generate_price_range_between_dates(start, end, country, property_type='apartment'):
+
+    query = SpiderOutputHistory.select(SpiderOutputHistory.rent, SpiderOutputHistory.created_at).where(
+        SpiderOutputHistory.country == country,
+        SpiderOutputHistory.property_type == property_type,
+        SpiderOutputHistory.rent.is_null(False),
+        SpiderOutputHistory.created_at.between(start, end)
+    ).distinct(SpiderOutputHistory.external_link)
+
+    all_data = [{'rent': x.rent, 'created_at': x.created_at}
+                for x in query if x.rent]
+
+    # make avg rent for each day
+    avg_rent = {}
+    for x in all_data:
+        date = x['created_at'].strftime('%Y-%m-%d')
+        if date not in avg_rent:
+            avg_rent[date] = []
+        avg_rent[date].append(x['rent'])
+
+    avg_rent = [{'date': x, 'avg_rent': sum(
+        avg_rent[x]) / len(avg_rent[x])} for x in avg_rent]
+
+    # clear avg_rent from outliers (rents that are 2 times higher or lower than average) just make them equal to average
+    for i in range(len(avg_rent)):
+        if i == 0 or i == len(avg_rent) - 1:
+            continue
+
+        if avg_rent[i]['avg_rent'] > avg_rent[i - 1]['avg_rent'] * 2 or avg_rent[i]['avg_rent'] < avg_rent[i - 1]['avg_rent'] / 2:
+            avg_rent[i]['avg_rent'] = avg_rent[i - 1]['avg_rent']
+        
+
+    return avg_rent
