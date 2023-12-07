f0bb3f8f4e63e48363e9b1ccb46715b2be15e3ff
Yatsyuk Julia Romanivna <yulya.yatsyuk@gmail.com>
2023-12-07 02:58:17 +0200

    3 pages update

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -1,1 +1,26 @@
 // js of search page
+const citySelect = document.getElementById("city");
+citySelect.disabled = false;
+
+const propertyTypeSelect = document.getElementById("property_type");
+propertyTypeSelect.disabled = false;
+
+function handleChange() {
+    const input = document.querySelector("#prices");
+    const selectedOption = input.options[input.selectedIndex];
+    const value = selectedOption ? selectedOption.value : input.value;
+    return value;
+}
+
+function handleChange() {
+    const input = document.querySelector("#square_meters");
+    const selectedOption = input.options[input.selectedIndex];
+    const value = selectedOption ? selectedOption.value : input.value;
+    return value;
+  }
+  
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    const price = handleChange();
+    // Виконати пошук з ціною price
+});
--- app/static/js/timeline.js
+++ app/static/js/timeline.js
@@ -1,1 +1,10 @@
 // js of timeline page
+const input = document.querySelector("input[name='date']");
+const date = input.value;
+
+// Зберігаємо дату у базу даних
+const citySelect = document.getElementById("city");
+citySelect.disabled = false;
+
+const propertyTypeSelect = document.getElementById("property_type");
+propertyTypeSelect.disabled = false;
